f9acee62e3aaead8123364c59db506dd603a8f69
ainunfauzi <44285058+ainunfauzi@users.noreply.github.com>
2019-12-16 10:05:30 +0700

    Update data1.php

--- application/views/upload/data1.php
+++ application/views/upload/data1.php
@@ -19,6 +19,51 @@
     <?php $this->load->view('menu');?> <!--Include menu-->
       <div class="col-md-12">
         <div class="row">
+          <center>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+              <!-- Indicators -->
+              <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+                <li data-target="#myCarousel" data-slide-to="4"></li>
+                <li data-target="#myCarousel" data-slide-to="5"></li>
+              </ol>
+
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner">
+                <div class="item active">
+                  <img src="<?php echo base_url().'assets/img/utm.jpg'?>" alt="Universitas Trunojoyo Madura" style="width: 450px;height: 400px">
+                </div>
+                <div class="item">
+                  <img src="<?php echo base_url().'assets/img/itb.jpg'?>" alt="Institute Teknologi Bandung" style="width: 450px;height: 400px">
+                </div>
+                <div class="item">
+                  <img src="<?php echo base_url().'assets/img/unm.jpg'?>" alt="Universitas Negeri Malang" style="width: 450px;height: 400px">
+                </div>
+                <div class="item">
+                  <img src="<?php echo base_url().'assets/img/unair.jpg'?>" alt="Universitas Negeri Airlangga" style="width: 450px;height: 400px">
+                </div>
+                <div class="item">
+                  <img src="<?php echo base_url().'assets/img/ipb.jpg'?>" alt="Institute Pertanian Bandung" style="width: 450px;height: 400px">
+                </div>
+                <div class="item">
+                  <img src="<?php echo base_url().'assets/img/uny.jpg'?>" alt="Universitas Negeri Yogyakarta" style="width: 450px;height: 400px">
+                </div>
+              </div>
+
+              <!-- Left and right controls -->
+              <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only"></span>
+              </a>
+              <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only"></span>
+              </a>
+            </div>
+          </center>
           <center><h2 style="color: white">Data Universitas</h2></center>
           <table class="table table-striped table-bordered">
             <tr>
